29c9bfbeb16faad9ff00444e71b7b5d0578c337d
BiancaTr <48147033+BiancaTr@users.noreply.github.com>
2020-03-23 19:03:09 +0200

    Laboratoare

---
+++ ClassLibraryNetCore/ModelContext.cs
@@ -0,0 +1,40 @@
+﻿using ClassLibraryNetCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Text;
+namespace ClassLibraryNetCore.Model
+{
+    public class ModelContext : DbContext
+    {
+
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Person> People { get; set; }
+        public DbSet<Artist> Artists { get; set; }
+        public DbSet<Album> Albums { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=DESKTOP-C9LQLRG\\SQLEXPRESS;Initial Catalog=EfCore2020;Integrated Security=True");
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<AlbumArtist>()
+                  .HasKey(a => new { a.AlbumId, a.ArtistId });
+
+            modelBuilder.Entity<AlbumArtist>()
+                .HasOne(a => a.Artist)
+                .WithMany(a => a.AlbumArtists)
+                .HasForeignKey(a => a.ArtistId);
+
+            modelBuilder.Entity<AlbumArtist>()
+                .HasOne(a => a.Album)
+                .WithMany(a => a.AlbumArtist)
+                .HasForeignKey(a => a.AlbumId);
+        }
+    }
+}
