3eb18a562f7940a235f97b71c4784f42171f774a
BiancaTr <48147033+BiancaTr@users.noreply.github.com>
2020-05-06 18:06:23 +0300

    Laboratorul 10

---
+++ Laborator 10 - ASP_RP/Asp_Traistar_Bianca_Rp/Asp_Traistar_Bianca_Rp/Data/Asp_Traistar_Bianca_RpContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Asp_Traistar_Bianca_Rp.Models;
+
+namespace Asp_Traistar_Bianca_Rp.Data
+{
+    public class Asp_Traistar_Bianca_RpContext : DbContext
+    {
+        public Asp_Traistar_Bianca_RpContext (DbContextOptions<Asp_Traistar_Bianca_RpContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Asp_Traistar_Bianca_Rp.Models.Movie> Movie { get; set; }
+    }
+}
